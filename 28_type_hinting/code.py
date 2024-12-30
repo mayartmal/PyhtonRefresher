5cd37128cf34b30ca7fcb9f5415aec4dbcee4072
mayartmal <mayartmal@gmail.com>
2024-12-30 18:31:02 +0400

    type hinting

---
+++ 28_type_hinting/code.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def list_avg(sequence: List) -> float:
+    return sum(sequence)/len(sequence)
+
+list_avg([1, 2, 3])
+
+
+
+class Book:
+    pass
+
+class BookShelf:
+    def __init__(self, books: List[Book]):
+        self.books = books
+
+    def __str__(self) -> str:
+        return  f"Book shelf with {len(self.books)} books"
+
+    
