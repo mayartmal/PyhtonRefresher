c3f595a2d32e0bfd47890b12b3973c8e51152b09
mayartmal <mayartmal@gmail.com>
2025-01-01 19:49:08 +0400

    mutability

---
+++ 38_mutability/code.py
@@ -0,0 +1,10 @@
+a = ()
+b = ()
+
+a = a + (15,)
+
+print(id(a))
+print(id(b))
+
+print(a)
+print(b)
---
+++ 38_mutability/code2.py
@@ -0,0 +1,10 @@
+a = []
+b = a
+
+a.append(35)
+
+print(id(a))
+print(id(b))
+
+print(a)
+print(b)
